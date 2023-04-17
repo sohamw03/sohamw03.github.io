b43a27f285222dbd467c62c93fea136fe80afaba
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-04-18 02:55:24 +0530

    Improved button touch input.

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,31 +1,28 @@
-function insert(num)
-{
-    document.getElementById('ip').value = document.getElementById('ip').value + num;
+function insert(num) {
+    document.getElementById("ip").value = document.getElementById("ip").value + num;
 }
 
-function equal()
-{
-    let res = document.getElementById('ip').value;
-    if(res)
-    {
-        document.getElementById('output').innerHTML = eval(res);
+function equal() {
+    let res = document.getElementById("ip").value;
+    if (res) {
+        document.getElementById("output").innerHTML = eval(res);
     }
 }
 
 var andar = document.getElementById("ip");
 andar.addEventListener("keydown", function (e) {
-    if(e.key === "Enter")
-    {
+    if (e.key === "Enter") {
         equal();
     }
 });
 
-const button = document.querySelector('.but');
+const button = document.querySelector(".but");
 
-button.addEventListener('touchstart', function() {
-  this.classList.add('active');
+button.addEventListener("touchmove", function (event) {
+    event.preventDefault();
+    this.classList.add("active");
 });
 
-button.addEventListener('touchend', function() {
-  this.classList.remove('active');
+button.addEventListener("touchend", function () {
+    this.classList.remove("active");
 });
