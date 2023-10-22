1c72bd33de90004e46938b5b24a8e04081ac1b70
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-10-23 02:46:23 +0530

    Fix : Set prefix to empty string

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 let assetPrefix = ``;
-let basePath = `/`;
+let basePath = ``;
 
 if (isGithubActions) {
   // trim off `<owner>/`
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
+  assetPrefix = ``;
+  basePath = ``;
 }
 
 const nextConfig = {
