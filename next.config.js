84533903998099659c6212785ea7ffac345966f0
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-10-23 02:25:14 +0530

    About section completed and Project sectinon start

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
 /** @type {import('next').NextConfig} */
-const assetPrefix = `/`;
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+const assetPrefix = ``;
 const basePath = `/`;
 
-const nextConfig = {
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  assetPrefix = `/`;
+  basePath = `/`;
+  const nextConfig = {
     assetPrefix: assetPrefix,
     basePath: basePath,
     output: "export",
+  };
+}
+
+const nextConfig = {
+  output: "export",
 };
 
 module.exports = nextConfig;
