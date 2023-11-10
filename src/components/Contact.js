8f513642eeb39d0b1d895ce9a1f0e2e2f408166c
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-11-11 02:02:24 +0530

    Fix : Pulse animation on contact modal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,30 @@
 "use client";
 import styles from "@/app/page.module.css";
-import { useEffect } from "react";
-import $ from "jquery";
+import { useEffect, useRef } from "react";
+import { ClickAwayListener } from "@mui/base/ClickAwayListener";
 
 export default function Contact() {
+  const terminalRef = useRef(null);
+
+  const closeModal = () => {
+    let contact = document.getElementsByClassName(`${styles.contact}`)[0];
+    let container = document.getElementsByClassName(`${styles.container}`)[0];
+
+    contact.style.setProperty("--offset", "3.4rem");
+    container.style.setProperty("--before-opacity", "0.5");
+  };
+
+  const openModal = () => {
+    let contact = document.getElementsByClassName(`${styles.contact}`)[0];
+    let container = document.getElementsByClassName(`${styles.container}`)[0];
+
+    contact.style.setProperty("--offset", "20rem");
+    container.style.setProperty("--before-opacity", "0");
+  };
+
   useEffect(() => {
-    // Event handler for Contact
-    let terminal = document.getElementsByClassName(`${styles.terminal}`)[0];
+    const terminal = terminalRef.current;
+    // let terminal = document.getElementsByClassName(`${styles.terminal}`)[0];
     let contact = document.getElementsByClassName(`${styles.contact}`)[0];
     let container = document.getElementsByClassName(`${styles.container}`)[0];
 
@@ -22,17 +40,19 @@ export default function Contact() {
   return (
     <>
       <section className={styles.contact}>
-        <section className={styles.terminal}>
-          <div className={styles.container}>
-            <div className={styles.bg}></div>
-            <button className={styles.contact_head_btn}>
-              <h2>Contact me</h2>
-              <span>Spaces: 2</span>
-              <span>UTF-8</span>
-              <span>▼</span>
-            </button>
-          </div>
-        </section>
+        <ClickAwayListener onClickAway={closeModal}>
+          <section className={styles.terminal} ref={terminalRef}>
+            <div className={styles.container}>
+              <div className={styles.bg}></div>
+              <button className={styles.contact_head_btn} onClick={openModal}>
+                <h2>Contact me</h2>
+                <span>Spaces: 2</span>
+                <span>UTF-8</span>
+                <span>▼</span>
+              </button>
+            </div>
+          </section>
+        </ClickAwayListener>
       </section>
       <button className={styles.terminal_overlay} aria-label="Close Private Preview form"></button>
     </>
