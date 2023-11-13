38b636b623bdea5d93a02f601a63cbc2c94e53ab
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-11-14 00:31:53 +0530

    Feature : Contact Form modal ready with animation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,7 @@ import { useEffect, useRef, useState } from "react";
 
 export default function Contact() {
   const [isOpen, setIsOpen] = useState(false);
+  const [contactOffset, setContactOffset] = useState("3.3rem");
 
   // Capture elements
   const contactRef = useRef(null);
@@ -36,8 +37,9 @@ export default function Contact() {
     terminal_overlayRef.current.style.setProperty("pointer-events", "none");
 
     // Handle borders
-    contactRef.current.style.setProperty("--gradient-inset", "0px");
-    contactHeadBtnRef.current.style.setProperty("--contact_head_btn-border-size", "1px");
+    contactRef.current.style.setProperty("--border-anim-opacity", "0");
+    contactRef.current.style.setProperty("--border-offset", "-1px");
+    contactHeadBtnRef.current.style.setProperty("--border-color", "#2e3c51");
   };
 
   const openModal = () => {
@@ -54,32 +56,33 @@ export default function Contact() {
     terminal_overlayRef.current.style.setProperty("pointer-events", "auto");
 
     // Handle borders
-    contactRef.current.style.setProperty("--gradient-inset", "0px");
-    contactHeadBtnRef.current.style.setProperty("--contact_head_btn-border-size", "1px");
+    contactRef.current.style.setProperty("--border-anim-opacity", "0");
+    contactRef.current.style.setProperty("--border-offset", "0px");
+    contactHeadBtnRef.current.style.setProperty("--border-color", "#2e3c51");
   };
 
   useEffect(() => {
     const handleMouseOver = (e) => {
       e.stopPropagation();
       if (!isOpen) {
-        contactRef.current.style.setProperty("--offset", "3.8rem");
+        setContactOffset("3.8rem");
         containerRef.current.style.setProperty("--before-opacity", "1");
         contactRef.current.style.setProperty("--animation-playpause", "running");
         terminalRef.current.style.setProperty("--border-x-position", "100%");
-        contactRef.current.style.setProperty("--gradient-inset", "-1px");
-        contactHeadBtnRef.current.style.setProperty("--contact_head_btn-border-size", "0px");
+        contactRef.current.style.setProperty("--border-anim-opacity", "1");
+        contactHeadBtnRef.current.style.setProperty("--border-color", "#2e3c5100");
       }
     };
 
     const handleMouseOut = (e) => {
       e.stopPropagation();
       if (!isOpen) {
-        contactRef.current.style.setProperty("--offset", "3.4rem");
+        setContactOffset("3.3rem");
         containerRef.current.style.setProperty("--before-opacity", "0.5");
         contactRef.current.style.setProperty("--animation-playpause", "paused");
         terminalRef.current.style.setProperty("--border-x-position", "0%");
-        contactRef.current.style.setProperty("--gradient-inset", "0px");
-        contactHeadBtnRef.current.style.setProperty("--contact_head_btn-border-size", "1px");
+        contactRef.current.style.setProperty("--border-anim-opacity", "0");
+        contactHeadBtnRef.current.style.setProperty("--border-color", "#2e3c51");
       }
     };
 
@@ -95,7 +98,7 @@ export default function Contact() {
   }, [isOpen]);
   return (
     <>
-      <section className={styles.contact} ref={contactRef} style={{ "--offset": isOpen ? "20rem" : "3.4rem" }}>
+      <section className={styles.contact} ref={contactRef} style={{ transform: isOpen ? "none" : `translateY(calc(100% - ${contactOffset}))` }}>
         <ClickAwayListener onClickAway={closeModal}>
           <section className={styles.terminal} ref={terminalRef}>
             <div className={styles.container} ref={containerRef}>
@@ -106,6 +109,38 @@ export default function Contact() {
                 <span>UTF-8</span>
                 <span>▼</span>
               </button>
+              <form className={`${styles.contact_body} p-6 flex items-center flex-wrap justify-stretch gap-x-6 gap-y-4`}>
+                <p>I will try to get back to you at the earliest. Enter your details to contact me.</p>
+                <div className="flex flex-col justify-start">
+                  <div className={`${styles.form_control_wrapper} flex items-end gap-2`}>
+                    <label htmlFor="request-name">Name</label>
+                    <span className="text-[#333e4f]">
+                      ......
+                      <span className="max-md:hidden">.......</span>
+                    </span>
+                    <input id="request-name" type="text" name="name" spellCheck="false" autoComplete="name" autoCapitalize="words" maxLength="128" required="" className={`${styles.form_control} flex-1 py-2 -mb-1`} placeholder="{Enter}" />
+                  </div>
+                  <div className={`${styles.form_control_wrapper} flex items-end gap-2`}>
+                    <label htmlFor="request-email">Email</label>
+                    <span className="text-[#333e4f]">
+                      .....
+                      <span className="max-md:hidden">.......</span>
+                    </span>
+                    <input id="request-email" type="email" name="email" required="" maxLength="128" autoComplete="email" className={`${styles.form_control} flex-1 py-2 -mb-1`} placeholder="{Enter}" />
+                  </div>
+                  <div className={`${styles.form_control_wrapper} flex items-end gap-2`}>
+                    <label htmlFor="request-email">Message</label>
+                    <span className="text-[#333e4f]">
+                      ...
+                      <span className="max-md:hidden">.......</span>
+                    </span>
+                    <input id="request-msg" type="text" name="msg" maxLength="128" className={`${styles.form_control} flex-1 py-2 -mb-1`} placeholder="{Enter}" />
+                  </div>
+                </div>
+                <button type="submit" title="Submit" className={`${styles.button_primary} ${styles.button} flex-shrink opacity-1 -translate-x-1 pointer-events-auto hover:bg-white`}>
+                  Send
+                </button>
+              </form>
             </div>
           </section>
         </ClickAwayListener>
