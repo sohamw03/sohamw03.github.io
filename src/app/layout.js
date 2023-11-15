c4d58d66ef2735d57ffa7f28bbff5d0fc0a29eb9
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2023-11-15 22:10:12 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
-import "./globals.css";
+import React from "react";
 import { Inter } from "next/font/google";
+import Head from "next/head";
+import styles from "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+const metadata = {
   title: "Soham's Tech Odyssey : Navigating the Digital Universe",
   description: "Embark on a digital odyssey with Soham as he showcases his remarkable tech journey, innovative creations, and expertise. Explore the universe of code, creativity, and problem-solving on this inspiring tech voyage.",
 };
@@ -11,6 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="stylesheet" href={styles} />
+        <meta property="og:image" content="./6554de43848d20764128d9b8.png">
+<meta property="og:title" content="Portfoli o- Soham">
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
