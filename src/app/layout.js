2bc76c0543e182a45220dd86411277e4b6134a40
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-11-25 00:32:45 +0530

    Refactor layout.js and update metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { Inter } from "next/font/google";
-import Head from "next/head";
-import styles from "./globals.css";
 import { GlobalProvider } from "@/context/GlobalContext";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const metadata = {
+export const metadata = {
   title: "Soham's Tech Odyssey : Navigating the Digital Universe",
   description: "Embark on a digital odyssey with Soham as he showcases his remarkable tech journey, innovative creations, and expertise. Explore the universe of code, creativity, and problem-solving on this inspiring tech voyage.",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://sohamw03.github.io",
+    siteName: "Soham's Tech Odyssey : Navigating the Digital Universe",
+    title: "Soham's Tech Odyssey : Navigating the Digital Universe",
+    description: "Embark on a digital odyssey with Soham as he showcases his remarkable tech journey, innovative creations, and expertise. Explore the universe of code, creativity, and problem-solving on this inspiring tech voyage.",
+    images: "./6554de43848d20764128d9b8.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="stylesheet" href={styles} />
-        <meta property="og:image" content="./6554de43848d20764128d9b8.png" />
-        <meta property="og:title" content="Portfolio - Soham" />
-      </Head>
       <body className={inter.className}>
         <GlobalProvider>{children}</GlobalProvider>
       </body>
